572b34a5a621d27eb398247af9963e0690e476f5
hikio-17 <fajri010217@gmail.com>
2023-05-15 01:58:53 -0700

    protection router

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -3,8 +3,35 @@ import Navigation from './Navigation';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
+import Register from '../pages/Register';
+import Login from '../pages/Login';
 
-function ContactApp() {
+class ContactApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      authedUser: null
+    }
+  }
+
+  render() {
+    if (this.state.authedUser === null) {
+      return (
+        <div className="contact-app">
+          <header className="contact-app__header">
+            <h1>Aplikasi Kontak</h1>
+          </header>
+
+          <main>
+            <Routes>
+              <Route path="*" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </main>
+        </div>
+      )
+    }
     return (
       <div className="contact-app">
         <header className="contact-app__header">
@@ -20,6 +47,7 @@ function ContactApp() {
         </main>
       </div>
     );
+  }
 }
 
 export default ContactApp;
